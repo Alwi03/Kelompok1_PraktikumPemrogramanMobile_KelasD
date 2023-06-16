d7dd15e5357215d5be7b2c6e96bca9e087c94b60
YusrilIsmailAzi <aziyusril30@gmail.com>
2023-06-16 22:54:25 +0700

    KELOMPOK1
    KELOMPOK1

---
+++ app/src/main/java/ppm/b/kelompok4/tokoelektronik/screens/AboutScreen.kt
@@ -0,0 +1,53 @@
+package ppm.b.kelompok4.tokoelektronik.screens
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import ppm.b.kelompok4.tokoelektronik.R
+
+@Composable
+fun AboutScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+            Image(
+                painter = painterResource(id = R.drawable.cloudtechlogo),
+                contentDescription = "logo")
+        }
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text="Created By",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterHorizontally).padding(bottom = 7.dp)
+            )
+            Text(
+                text = "Yusril Ismail Azi",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Muhammad Alwi R",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Anjara D Nissa",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Sulthan Jihad",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Gigih Dwi Saputra",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
